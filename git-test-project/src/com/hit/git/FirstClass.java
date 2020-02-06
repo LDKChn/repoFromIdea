64374e9ce3dbb70f9006c8b5448b70ac5fd57c1a
LDK <hitkarl@163.com>
2020-02-06 20:55:54 +0800

    from idea to add a project to git

---
+++ git-test-project/src/com/hit/git/FirstClass.java
@@ -0,0 +1,8 @@
+package com.hit.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("I am Karl");
+    }
+}
